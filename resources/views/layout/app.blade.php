a5bf15a1432f352fe476a70d9e8192499af0d960
Mustafa-Elfrmawy <mustafaelfrmawy@gmail.com>
2025-05-09 02:27:32 +0300

    edit for deploying

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'title')</title>
-    <link href="{{ asset('bootstrap-5.3.3-dist/css/bootstrap.css') }}" rel="stylesheet">
-    <link href="{{ asset('pro.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('bootstrap-5.3.3-dist/css/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('pro.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 </head>
 <style>
@@ -92,8 +92,8 @@
     @yield('content')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('mainStyle/js_frist.js') }}"></script>
-    <script src="{{ asset('jquery-3.7.1.min.js') }}"></script>
+    <script src="{{ secure_asset('mainStyle/js_frist.js') }}"></script>
+    <script src="{{ secure_asset('jquery-3.7.1.min.js') }}"></script>
 
 </body>
 
